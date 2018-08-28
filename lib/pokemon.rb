fedef8c99dd1ec853d4a5ee84896b33c573b6353
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 18:00:47 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,9 +17,6 @@ class Pokemon
     pokemons= db.execute("SELECT * FROM pokemon WHERE id=?",id).flatten
     Pokemon.new(id: pokemons[0], name: pokemons[1],type: pokemons[2], db: db)
   end
-  def bonus(hp=60)
-    
-    
-  end
+
   
 end
